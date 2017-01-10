708753cfdacf14cf57a3e773d979ca0ea601d058
marysiaK <marysia.kolaczynska@gmail.com>
2017-01-10 14:47:31 +0100

    buttons

--- scripts.js
+++ scripts.js
@@ -10,5 +10,20 @@ $(function(){
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
-	}	
+	}
+	
+    $('#previous-btn').click(function() {
+       changeSlide();
+    });
+
+    function changeSlideRight() {
+         carouselList.animate({'marginLeft': 400}, 500, moveFirstSlide);
+    }
+
+
+    $('#next-btn').click(function() {
+       changeSlideRight();
+    });
+
+
 });
