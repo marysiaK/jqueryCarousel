1acf0bc88fcf392818aee299f1e1bbdcec49d30f
marysiaK <marysia.kolaczynska@gmail.com>
2017-01-23 20:23:55 +0100

    poprawiona karuzela

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 $(function(){
 	var carouselList = $("#carousel ul");
-	setInterval(changeSlide, 3000);
+	//setInterval(changeSlide, 3000);
 	
 	function changeSlide() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide());
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 	};
 	function moveFirstSlide() {
 		var firstItem = carouselList.find("li:first");
@@ -11,13 +11,20 @@ $(function(){
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	};
+	function moveLastSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:-400});
+	};
 
 	$('#previous-btn').click(function() {
-		carouselList.animate({'marginleft':+400}, 500, moveFirstSlide());
+		moveLastSlide()
+		carouselList.animate({'marginLeft':0}, 500);
 	});
 
 	$('#next-btn').click(function() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide());
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 	});
 
 });
